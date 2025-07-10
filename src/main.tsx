35166cecc2de06e1b473c2f8e75b03cdb4c15ee9
[dyad] <git@dyad.sh>
2025-07-10 00:39:40 -0700

    Reverted all changes back to version d61fb8c8dd21a9b57cb3e23d3dd3ae08a9fc7abc

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
-import "./amplify-config.ts"; // Import Amplify configuration
-import { AuthProvider } from "./components/AuthProvider.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>
-);
+createRoot(document.getElementById("root")!).render(<App />);
