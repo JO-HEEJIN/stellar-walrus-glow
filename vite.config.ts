d805f77d6b946b770d1a44b31a7c03c8384a90b2
[dyad] <midmost44@gmail.com>
2025-07-10 22:56:16 -0700

    [dyad] Fixed Rollup module issue for Vercel deployment - wrote 2 file(s)

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,12 @@
 import { defineConfig } from "vite";
-import dyadComponentTagger from "@dyad-sh/react-vite-component-tagger";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import dyadComponentTagger from "@dyad-sh/react-vite-component-tagger";
 
 export default defineConfig(() => ({
-  server: {
-    host: "::",
-    port: 8080,
+  build: {
+    rollupOptions: {
+      cache: false // 캐시 비활성화
+    }
   },
   plugins: [dyadComponentTagger(), react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  optimizeDeps: {
-    exclude: ['@aws-amplify/utils'],
-    // Removed aws-amplify related includes
-  },
 }));
